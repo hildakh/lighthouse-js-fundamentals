71713bea5aa4ff8ad889053154a7a85d91c50153
hildakh <h_khajehamiri@yahoo.com>
2019-09-19 04:05:13 +0000

    Add more conditional examples

---
+++ conditionals.js
@@ -0,0 +1,18 @@
+/*let raining = true;
+if (raining) {
+console.log("don't forgt to take your umbrella");
+}
+*/
+let raining = true;
+let cold = true;
+
+if (raining) {
+console.log("Don't forget your umbrella");
+}
+
+if (cold) {
+console.log("Make sure you pick out a scarf");
+}
+console.log("Now you're ready to go outise!");  
+
+
