2b76c16e02cc7aacdcd385b4c70fbc31df8e74cb
hildakh <h_khajehamiri@yahoo.com>
2019-09-21 19:42:28 +0000

    laughitoffchallenge udacity

---
+++ laughitoff.js
@@ -0,0 +1,8 @@
+function laugh(num) {
+    let result = "";
+    for (var i = 0; i < num; i++ ) {
+        result = result + "ha";
+        }
+    return result + "!";
+    }
+console.log(laugh(1));
