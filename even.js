c0adc1af0604eb86ebe02e9aff0c75d4602fc294
hildakh <h_khajehamiri@yahoo.com>
2019-09-23 03:47:10 +0000

    isEven function was fun

---
+++ even.js
@@ -0,0 +1,11 @@
+	let isEven = function(num) {
+		return num % 2 === 0;
+	}
+	console.log(isEven(7));
+	console.log(isEven(8));
+
+	let ifteneven = isEven(10);
+	let ifthirteeneven = isEven(13);
+
+	console.log(ifteneven);
+	console.log(ifthirteeneven);
