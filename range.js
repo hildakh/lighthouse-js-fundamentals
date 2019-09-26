33f0a898a7b1d5d4bd7859bc3d1fd10eaaaa0d91
hildakh <h_khajehamiri@yahoo.com>
2019-09-26 05:36:35 +0000

    function and loop - looping through a range of numbers to add them to a new loop

---
+++ range.js
@@ -0,0 +1,14 @@
+function range(start, end, step) {
+	let numRange = [];
+	if (start !== "undefined" && end !== "undefined" && step !== "undefined" && end > start && 0 < step) {
+	for(var i = start; i <= end; i += step) {
+		numRange.push(i);
+	}
+	return numRange;
+}	else {
+	return [];
+}
+}
+console.log(range(0,10,2));
+console.log(range(10,30,5));
+console.log(range(-5, 2, 3));
