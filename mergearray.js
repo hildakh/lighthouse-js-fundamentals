188a2863139aa2f78e08561094aae68e6ff861df
hildakh <h_khajehamiri@yahoo.com>
2019-10-10 05:07:16 +0000

    merge two number arrays and arrange them in ascending order

--- mergearray.js
+++ mergearray.js
@@ -1,8 +1,6 @@
 function merge(a, b) {
-    let mergedFun = [];
-    mergedFun[0] = a;
-    mergedFun[1] = b;
-    mergedFun.sort(function(a, b) {return a - b});
+    let mergedFun = a.concat(b);
+    return mergedFun.sort();
 }
 console.log(merge([ 4, 5, 6 ], [ 1, 2, 3, 4 ]), "=?", [ 1, 2, 3, 4, 4, 5, 6 ]);
 console.log(merge([ 4 ], [ 2, 5, 8 ]), "=?", [ 2, 4, 5, 8 ]);
